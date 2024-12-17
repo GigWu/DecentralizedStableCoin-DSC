8d3cae166f52a056a2f4c548ca902764372edf6d
Yebu <giglian.wu@outlook.com>
2024-12-17 20:29:08 +0100

    Handler for fuzz testing

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,43 @@
+//SPDX-LICENSE-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+contract Handler is Test {
+    DSCEngine dscEngine;
+    DecentralizedStableCoin dsc;
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+
+    uint256 MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        dscEngine = _dscEngine;
+        dsc = _dsc;
+
+        address[] memory collateralTokens = dscEngine.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+    }
+
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+        dscEngine.depositCollateral(address(collateral), amountCollateral);
+    }
+
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        }
+        return wbtc;
+    }
+}
